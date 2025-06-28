27a868395c122d619703712cdc0156face9b9d70
Stefan-Comandant <stefancomandant@gmail.com>
2025-06-28 11:40:59 +0300

    add popup window feature

--- lua/stefan-plugin/init.lua
+++ lua/stefan-plugin/init.lua
@@ -12,11 +12,23 @@ local open_ter_win = "<cmd>10split | ter <CR><C-W>r"
 vim.api.nvim_set_keymap("n", "<leader>ter",open_ter_win, {})
 vim.api.nvim_set_keymap("n", "<leader>foo","", {
     callback = function ()
-        print(vim.fn.expand("%:p"))
-    end
+        -- print(scheisse)
+        -- print(vim.fn.expand("%:p"))
+        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+
+        -- print(lines[1]);
+        for _, v in pairs(lines) do
+            print(v .. "\n")
+        end
+    end,
 })
-local print_file_name = require(modpath .. '.module.init')
+local print_file_name = require(modpath .. '.module.print_file_name')
+
+-- vim.fn.jobstart("neofetch");
+
+
 
 return {
     do_foo = print_file_name,
+    build = require(modpath .. ".module.project_build")
 }
